017db43a45589a030dec0147d8fd0acfe1a51e32
Rustam Aliyev <rustam.aliyev889@gmail.com>
2023-10-30 18:43:46 +0000

    added ability to sort movies based upon rating

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import { fetchMovies } from './api';
 import MoviesList from './MoviesList';
 import InfiniteScroll from 'react-infinite-scroll-component'; // Library 4 infinite scrolling
-
 function App() {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
+  const [sortByRating, setSortByRating] = useState(false); // State for sorting
 
   const fetchMoreData = () => {
     // Function to load more movies
@@ -22,9 +22,26 @@ function App() {
     });
   }, []);
 
+  const toggleSort = () => {
+    setSortByRating(!sortByRating);
+    // Sort the movies based on rating
+    const sortedMovies = [...movies];
+    if (sortByRating) {
+      sortedMovies.sort((a, b) => b.vote_average - a.vote_average); // Sort in descending order
+    } else {
+      sortedMovies.sort((a, b) => a.vote_average - b.vote_average); // Sort in ascending order
+    }
+    setMovies(sortedMovies);
+  };
+
   return (
     <div className="App">
-      <h1>Movie Database</h1>
+      <header className='header'>
+        <h1>Movie Database</h1>
+      </header>
+      <button onClick={toggleSort}>
+        {sortByRating ? 'Sort by Rating (High to Low)' : 'Sort by Rating (Low to High)'}
+      </button>
       <InfiniteScroll
         dataLength={movies.length} // To prevent infinite rendering
         next={fetchMoreData} // Function to load more data
